21e8a7e1052e4dffb157b733efdb84be99ecea67
avidrutham <divyamathur89@gmail.com>
2020-01-10 17:18:50 +0530

    add js code for filtered gallery

--- main.js
+++ main.js
@@ -4,3 +4,42 @@ const mobileNav = document.querySelector(".mobile-nav");
 toggleButton.addEventListener("click", function(event) {
   mobileNav.classList.toggle("active");
 });
+
+// convert showcase filter buttons and columns to iterable array
+var showcaseMenuBtn = Array.prototype.slice.call(
+  document.querySelectorAll(".showcase-filter__item")
+);
+
+var showcaseColumn = Array.prototype.slice.call(
+  document.querySelectorAll(".showcase-column")
+);
+
+function filterSelection(name) {
+  if (name === "all") {
+    showcaseColumn.forEach(function(column) {
+      column.classList.add("showcase-show");
+    });
+  } else {
+    for (var i = 0; i < showcaseColumn.length; i++) {
+      if (showcaseColumn[i].classList.contains(name)) {
+        showcaseColumn[i].classList.add("showcase-show");
+      } else {
+        showcaseColumn[i].classList.remove("showcase-show");
+      }
+    }
+  }
+}
+
+// Highlight menu buttons on click
+for (var i = 0; i < showcaseMenuBtn.length; i++) {
+  showcaseMenuBtn[i].addEventListener("click", function(event) {
+    showcaseMenuBtn.forEach(function(button) {
+      button.classList.remove("active-link");
+    });
+    event.target.classList.add("active-link");
+    filterSelection(event.target.getAttribute("data-project-type"));
+  });
+}
+
+// show all items by default
+filterSelection("all");
