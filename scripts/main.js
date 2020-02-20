1f43eec9a51ba486d1e8cf500608825fd3c43042
avidrutham <divyamathur89@gmail.com>
2020-02-20 11:55:29 +0530

    add typewriter effect to header tagline

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const headerTagline = document.querySelector(".showcase-header__feature");
-const features = ["fullstack developer", "blogger", "coding enthusiast"];
-var index = 0;
+const features = ["developer", "blogger", "code geek"];
+const waitTime = headerTagline.getAttribute("data-wait");
 
 // convert showcase filter buttons and columns to iterable array
 const showcaseMenuBtn = Array.prototype.slice.call(
@@ -11,17 +11,58 @@ const showcaseColumn = Array.prototype.slice.call(
   document.querySelectorAll(".showcase-column")
 );
 
-// change feature in header tagline
+// Create class for typewriter effect in header tagline
+const TypeWriter = function(textElem, words, wait = 3000) {
+  this.textElem = textElem;
+  this.words = words;
+  this.wait = parseInt(wait, 10);
+  this.txt = "";
+  this.wordIndex = 0;
+  this.type();
+  this.isDeleting = false;
+};
 
-function showFeature() {
-  headerTagline.innerHTML = features[index];
-  if (index < features.length - 1) {
-    index++;
+// Type Method
+TypeWriter.prototype.type = function() {
+  // Get current index of words array
+  const current = this.wordIndex % this.words.length;
+
+  // Get the first word in array
+  const fullText = this.words[current];
+
+  // Check if deleting char
+  if (this.isDeleting) {
+    this.txt = fullText.substring(0, this.txt.length - 1);
   } else {
-    index = 0;
+    this.txt = fullText.substring(0, this.txt.length + 1);
   }
-}
 
+  // Insert txt output into html
+  this.textElem.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  // Set Type output speed
+  let typespeed = 300;
+
+  if (this.isDeleting) {
+    typespeed /= 2;
+  }
+
+  // check if current word is comletely typed
+  if (!this.isDeleting && this.txt === fullText) {
+    typespeed = this.wait;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.wordIndex++;
+
+    // Pause before resuming typing
+    typespeed = 300;
+  }
+
+  setTimeout(() => this.type(), typespeed);
+};
+
+// Filter Image gallery and show selected items
 function filterSelection(name) {
   if (name === "all") {
     showcaseColumn.forEach(function(column) {
@@ -52,6 +93,5 @@ for (var i = 0; i < showcaseMenuBtn.length; i++) {
 // show all items by default
 filterSelection("all");
 
-// display header tagline
-showFeature();
-setInterval(showFeature, 2000);
+// Initialize Typewriter instance
+new TypeWriter(headerTagline, features, waitTime);
